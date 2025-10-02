93b6ad6f41873571bb205802b4a3fa077d7b669e
Elisabeth Lichtie <elisabethlichtie99@gmail.com>
2025-10-02 18:03:30 -0500

    update to use component from folder

--- storage/ComponentsStackRefsApp.cs
+++ storage/ComponentsStackRefsApp.cs
@@ -6,21 +6,24 @@ class ComponentsStackRefsApp : Stack
     {
         var config = new Config();
         var baseName = config.Require("baseName");
-        var baseStackProjectName = config.Require("baseStackProjectName");
-        var baseStackName = config.Require("baseStackName");
+
+        //**ALT 1: get from stack reference
+        // var baseStackProjectName = config.Require("baseStackProjectName");
+        // var baseStackName = config.Require("baseStackName");
 
         // Build the full stack reference path: org/project/stack
-        var stackName = Pulumi.Deployment.Instance.StackName;
-        var orgName = Pulumi.Deployment.Instance.OrganizationName;
-        var baseStackFullName = $"{orgName}/{baseStackProjectName}/{baseStackName}";
+        // var stackName = Pulumi.Deployment.Instance.StackName;
+        // var orgName = Pulumi.Deployment.Instance.OrganizationName;
+        // var baseStackFullName = $"{orgName}/{baseStackProjectName}/{baseStackName}";
 
-        // Create reference to the base infrastructure stack
-        var baseStackRef = new StackReference(baseStackFullName);
+        // // Create reference to the base infrastructure stack
+        // var baseStackRef = new StackReference(baseStackFullName);
 
         // // Get the resource group name from the base stack's outputs
         // var resourceGroupName = stackOutput(baseStackRef, "ResourceGroupName");
 
-        // var resourceGroupName = config.Require("resourceGroupName");
+        //**ALT 2: get from environment
+        var resourceGroupName = config.Require("resourceGroupName");
 
         // Create storage resources using the custom component
         var storage = new StorageComponent("storage", new StorageComponentArgs
